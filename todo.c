8372b6abd45690ca301e69f56e4eba55bc7af8aa
reuteran <andreas.router@googlemail.com>
2013-06-05 20:37:36 +0200

    Added IDs to the ToDoItems so they can be deleted better later on

--- todo.c
+++ todo.c
@@ -14,6 +14,7 @@ typedef struct ToDoItem
 {	
 	char desc[MAX_DESC_LEN];
 	uint prio;
+	uint id;
 	struct ToDoItem *next;
 
 } ToDoItem;
@@ -21,8 +22,11 @@ typedef struct ToDoItem
 void printItems();
 void insertItem(char *prio, char *desc);
 void deleteItem(ToDoItem item);
-void init();
+void init(FILE *file);
 void quit(const char *message);
+int getNextId();
+void initIDs();
+FILE *initFile();
 
 
 
@@ -31,7 +35,7 @@ void quit(const char *message);
 
 /*Variables*/
 ToDoItem *firstItem;
-
+int ids[MAX_ITEMS];
 
 
 
@@ -41,10 +45,70 @@ void printItems()
 {
 	ToDoItem *current = firstItem;
 	while(current != NULL){
-		printf("Prio: %d \tDesc: %s\n",current->prio,current->desc);
+		printf("ID: %d \tPrio: %d \tDesc: %s\n",current->id,current->prio,current->desc);
 		current = current->next;
 	}
 
+
+}
+
+FILE *initFile()
+{
+	FILE *file = fopen(FILENAME,"r");
+
+	if(!file){
+		file = fopen(FILENAME,"w");
+		if(!file){
+			quit("Error creating file");
+		
+		}
+	}
+	return file;
+}
+
+void initIDs()
+{
+
+	int i;
+	int setIDs[MAX_ITEMS];
+
+	for(i = 0;i<MAX_ITEMS;i++){
+		setIDs[i] = 0;
+	}
+	ToDoItem *current = firstItem;
+
+	while(current !=NULL){
+		setIDs[current->id - 1] = 1;
+		current = current->next;
+	}
+	for(i = 0;i<MAX_ITEMS;i++){
+		ids[i] = setIDs[i];
+	}
+
+
+/*
+	char buffer[4];
+	int buf_id;
+	while(!fgets(buffer,4,file)){
+		sscanf(buffer,"%d",&buf_id);
+		setIDs[buf_id-1] = 1;
+	}
+*/
+
+}
+
+
+//Returns next id between 1 and 20.
+int getNextId()
+{
+	int i = 0;
+	for(i = 0;i<MAX_ITEMS;i++){
+		if(!ids[i]){
+			ids[i] = 1;
+			return i+1;
+		}
+	}
+	return 0;
 
 }
 
@@ -58,9 +122,18 @@ void quit(const char *message)
 	exit(1);
 }
 
+
+
+
+
+
 void insertItem(char *prio_c, char *desc_c)
 {
+
 	uint prio = atoi(prio_c);
+
+
+
 	if(prio > 100){
 		printf("Priority has to be in [0,100]\n");
 		exit(0);
@@ -68,13 +141,23 @@ void insertItem(char *prio_c, char *desc_c)
 	FILE *file = fopen(FILENAME,"a");
 
 	if(!file){
-		quit("Error reading file");
+		quit("Error opening file");
 	}
+
+	uint id = getNextId();
+	if(!id){
+		printf("List is full, delete an item before adding a new one\n");
+		exit(0);
+	}
+
 
 	char buffer[strlen(desc_c)+1];
 	strcpy(buffer,desc_c);
 
-	fprintf(file, "%d %s\n",prio,buffer);
+	if(fprintf(file, "%d %d %s\n",id,prio,buffer) < 0){
+		ids[id-1] = 0;
+		quit("Error writing to file");
+	}
 	
 
 	fclose(file);
@@ -82,43 +165,35 @@ void insertItem(char *prio_c, char *desc_c)
 }
 
 
-void init()
-{
-	FILE *file = fopen(FILENAME,"r");
 
-	if(!file){
-		if(fopen(FILENAME,"w")){
-			printf("No ToDos found!\n");
-			exit(0);
-		} else {
-			quit("Error creating file");
-		}
-
-		quit("Error opening file");
-	}
-
+void init(FILE *f)
+{	
+	FILE *file = f;
 	char buffer[96];
 
 	if(!fgets(buffer,95,file)){
-		if(feof){
-			printf("No ToDos found!\n");
-			exit(0);
+		if(!feof){
+			quit("Error reading file");	
 		}
-		quit("Error reading file");
+	} else {
+
+
+		firstItem = malloc(sizeof(ToDoItem));
+		sscanf(buffer,"%d %d %[^\n]", &firstItem->id, &firstItem->prio, firstItem->desc);
+
+		ToDoItem *last = firstItem;
+
+		while(fgets(buffer,128,file)){
+			ToDoItem *current = malloc(sizeof(ToDoItem));
+			sscanf(buffer,"%d %d %[^\n]",&current->id, &current->prio, current->desc);
+			last->next = current;
+			last = current;
+
+		}
+		fclose(file);
+		initIDs();
 	}
-	firstItem = malloc(sizeof(ToDoItem));
-	sscanf(buffer,"%d %[^\n]", &firstItem->prio, firstItem->desc);
 
-	ToDoItem *last = firstItem;
-
-	while(fgets(buffer,128,file)){
-		ToDoItem *current = malloc(sizeof(ToDoItem));
-		sscanf(buffer,"%d %[^\n]", &current->prio, current->desc);
-		last->next = current;
-		last = current;
-
-	}
-	fclose(file);
 
 
 }
@@ -126,8 +201,9 @@ void init()
 
 int main(int argc, char *argv[])
 {
+	FILE *file = initFile();
+	init(file);
 	if(argc == 1){
-		init();
 		printItems();
 	} else {
 		if(!strcmp(argv[1],"add")){
